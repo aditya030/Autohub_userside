03ac7128f5bc9db7b39aaaa826c7cbb5c057fbf9
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2024-06-08 17:22:52 +0530

    Constructed the Login Page, added images and updated the pubspec.yaml file

---
+++ lib/components/text_field_style.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TextFieldStyle {
+  // Border design when the textfield is not in focus.
+  static const loginfield = OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide(
+      color: Colors.black,
+      width: 2.0,
+    ),
+  );
+
+  // Border design when the textfield is in focus.
+  static const focussedLoginField = OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10),
+    ),
+    borderSide: BorderSide(
+      color: Colors.black,
+      width: 2.5,
+    ),
+  );
+}
---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Colors.white;
+  static const primaryColor = Colors.black;
+}
